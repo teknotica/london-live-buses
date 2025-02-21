051acf346956686c9ff4ce1967fb50c3357fa6ef
Silvia Rebelo <silvi04@gmail.com>
2025-02-21 16:32:49 +0000

    Update app.js

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('busesLiveApp', ['ngMap']);
 app.constant('CONFIG', {
     "transportAPI": {
         "url": "http://transportapi.com/v3/uk/bus",
-        "key": "44a684e1128915a5aeabdfbcae55b744",
+        "key": "YOUR_KEY",
         "app_id": "8fb7c55f"
     },
     "googleMapsAPI": {
